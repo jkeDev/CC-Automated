63673e1f45f3435fcbd3e835c0fd0b0a041289b9
jkeDev <52346246+jkeDev@users.noreply.github.com>
2022-10-24 21:34:18 +0200

    Rewritten log function in install.lua

--- install.lua
+++ install.lua
@@ -4,8 +4,14 @@ branch = branch or 'main'
 
 local basePath = ('https://raw.githubusercontent.com/%s/%s/'):format(repo, branch)
 
-local function log(...) io.stdout:write(string.format(...)) end
-local function err(...) io.stderr:write(string.format(...)) end
+local function log(lvl, ...)
+    (lvl > 0 and io.stderr or io.stdout)
+        :write(({[0] =
+            "[INFO ] ",
+            "[WARN ] ",
+            "[ERROR] "})[lvl]
+            .. string.format(...))
+end
 local function resolve(path, base)
     base = base or basePath .. 'src/'
     return base .. path
@@ -16,28 +22,28 @@ local function request(...)
     local triesLeft     = 3
     while (status ~= 200 or err ~= nil) and triesLeft > 0 do
         response.close()
-        err("[FAIL ] Could not fetch file /%s (%s)", path, err or tostring(status))
+        log(1, "Could not fetch file /%s (%s)", path, err or tostring(status))
         triesLeft = triesLeft - 1
         status, response = http.get(basePath .. path)
     end
     if status ~= 200 then
         response.close()
-        err("[ERROR] All retries failed...")
+        log(2, "All retries failed...")
         return nil
     else return response end
 end
 
-log("[INFO ] Fetching index from %s", resolve('index')) 
+log(0, "Fetching index from %s", resolve('index')) 
 local response = request('index')
 if response ~= nil then
     for _,path in response.readLine do
-        log("[INFO ] Downloading %s", path)
+        log(0, "Downloading %s", path)
         local source = request(path)
         local target = fs.open(path, 'w')
         if source == nil then
-            err("[ERROR] Could not read file %s...", path)
+            log(2, "Could not read file %s...", path)
         elseif type(target) ~= 'table' then
-            err("[ERROR] Could not open file %s...", path)
+            log(2, "Could not open file %s...", path)
             source.close()
         else
             target.write(source.readAll())
